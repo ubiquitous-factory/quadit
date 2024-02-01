fbdbed844b059e725c05981047551c6ed7f4c646
Anton Whalley <antonwhalley@yahoo.com>
2024-02-01 23:58:08 +0000

    add: build and sample restructure
    Signed-off-by: Anton Whalley <antonwhalley@yahoo.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-tag=v0.1.2
+tag=v0.1.3
 img=quay.io/ubiquitous-factory/quadit:$tag
-podman manifest create -a $img
 
-podman build --platform linux/amd64,linux/arm64  -f service.Dockerfile --manifest $img  .
+podman build --platform linux/amd64 -t $img-linux-amd64 -f service.Dockerfile .
+podman build --platform linux/arm64/v8 -t $img-linux-arm64 -f service.Dockerfile .
+
+podman push $img-linux-amd64 
+podman push $img-linux-arm64 
+
+podman manifest create $img $img-linux-amd64 $img-linux-arm64
 
 podman manifest push $img
